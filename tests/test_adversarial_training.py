5dce308118018820484581b43be6e97a0167b5f4
Jonathan Peck <jonathan.peck@ugent.be>
2024-12-29 22:52:06 +0100

    test adversarial training

---
+++ tests/test_adversarial_training.py
@@ -0,0 +1,13 @@
+from robusthub import threats
+from robusthub import models
+from robusthub import defenses
+
+def test_at(trainloader, testloader, device):
+    # load model
+    model = models.load('pytorch/vision', 'resnet18').to(device)
+
+    # initialize defense
+    defense = defenses.AdversarialTraining(trainloader, testloader, threats.Linf(.03), device=device)
+
+    # adversarially train
+    robust_model = defense.apply(model)
