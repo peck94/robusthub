2c4cd0d073f3e20d976faab1ad2bf494085176a6
Jonathan Peck <jonathan.peck@ugent.be>
2024-12-29 22:23:34 +0100

    basic PGD

--- robusthub/attacks/pgd.py
+++ robusthub/attacks/pgd.py
@@ -1,13 +1,37 @@
 import torch
+import torch.nn.functional as F
 
+from robusthub.models import Model
+from robusthub.threats import ThreatModel
 from robusthub.attacks.attack import Attack
 
 class ProjectedGradientDescent(Attack):
     """
     The projected gradient descent attack.
     """
-    def __init__(self, model):
-        super().__init__(model)
+    def __init__(self,
+                 model: Model,
+                 threat_model: ThreatModel,
+                 iterations: int = 100,
+                 device: torch.device = torch.device('cuda')):
+        super().__init__(model, threat_model)
+
+        self.iterations = iterations
+        self.device = device
     
-    def apply(self, x_data: torch.Tensor, y_data: torch.Tensor | None) -> torch.Tensor:
-        return x_data
+    def apply(self, x_data: torch.Tensor, y_data: torch.Tensor) -> torch.Tensor:
+        noise = torch.randn(x_data.shape).to(self.device)
+        x_tilde = self.threat.project(x_data, x_data + noise)
+
+        for _ in range(self.iterations):
+            samples = x_tilde.clone().detach().requires_grad_()
+            samples.retain_grad()
+
+            y_pred = self.model(samples)
+            loss = F.nll_loss(y_pred, y_data)
+            loss.backward()
+
+            deltas = torch.sign(samples.grad)
+            x_tilde = self.threat.project(x_data, x_tilde + deltas)
+            samples.grad.zero_()
+        return x_tilde
