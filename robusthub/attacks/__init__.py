b13a30e6d0630702f1576769a00dd049b64e76d1
Jonathan Peck <jonathan.peck@ugent.be>
2024-12-29 19:36:02 +0100

    imports

--- robusthub/attacks/__init__.py
+++ robusthub/attacks/__init__.py
@@ -1,1 +1,5 @@
 __all__ = ['attack']
+
+from robusthub.attacks.attack import Attack
+from robusthub.attacks.fgsm import FastGradientSignMethod
+from robusthub.attacks.pgd import ProjectedGradientDescent
--- robusthub/defenses/__init__.py
+++ robusthub/defenses/__init__.py
@@ -1,1 +1,5 @@
 __all__ = ['defense', 'adversarial_training', 'randomized_smoothing']
+
+from robusthub.defenses.defense import Defense
+from robusthub.defenses.adversarial_training import AdversarialTraining
+from robusthub.defenses.randomized_smoothing import RandomizedSmoothing
