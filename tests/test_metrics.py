982c01ae5a71473fbd3cec8efd44c74019c174c3
Jonathan Peck <jonathan.peck@ugent.be>
2024-12-30 20:16:05 +0100

    refactor

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -9,20 +9,12 @@ def test_at(testloader, device):
 
     # measure things
     accuracy = metrics.Accuracy()
-    time = metrics.Runtime()
-    memory = metrics.Memory()
 
     accs = []
-    times = []
-    mems = []
     for batch in testloader:
         x_data, y_data = batch
         x_data, y_data = x_data.to(device), y_data.to(device)
 
         accs.append(accuracy.compute(model, x_data, y_data))
-        times.append(time.compute(model, x_data, y_data))
-        mems.append(memory.compute(model, x_data, y_data))
     
     print(f'Accuracy: {np.sum(accs) / len(testloader):.2%}')
-    print(f'Runtime : {np.mean(times)}')
-    print(f'Memory  : {np.mean(mems)}')
