ba83863d1ce81224d3d6ed5694d09564f09095b2
Jonathan Peck <jonathan.peck@ugent.be>
2024-12-30 02:11:24 +0100

    composite threat models and clipping

--- robusthub/threats.py
+++ robusthub/threats.py
@@ -7,6 +7,8 @@ import numpy as np
 
 from abc import ABC, abstractmethod
 
+from typing import List
+
 class ThreatModel(ABC):
     """
     Abstract base class for threat models.
@@ -33,6 +35,46 @@ class ThreatModel(ABC):
             Perturbed samples projected back onto the feasible set.
         """
         pass
+
+class Composite(ThreatModel):
+    """
+    Sequentially compose multiple threat models.
+    """
+    def __init__(self, threat_models: List[ThreatModel]):
+        super().__init__()
+        self.threats = threat_models
+    
+    def project(self, x_orig: torch.Tensor, x_tilde: torch.Tensor) -> torch.Tensor:
+        """
+        Apply all given threat models.
+        """
+        x_proj = x_tilde
+        for threat in self.threats:
+            x_proj = threat.project(x_orig, x_proj)
+        return x_proj
+
+class Bounds(ThreatModel):
+    """
+    Simple bounds on the numerical values of the data vectors.
+
+    Parameters
+    ----------
+    lower_bound
+        Lower bound on the values. Specify :code:`None` for no lower bound.
+    
+    upper_bound
+        Upper bound on the values. Specify :code:`None` for no upper bound.
+    """
+    def __init__(self, lower_bound: float | None, upper_bound: float | None):
+        super().__init__()
+        self.lower = lower_bound
+        self.upper = upper_bound
+    
+    def project(self, x_orig: torch.Tensor, x_tilde: torch.Tensor) -> torch.Tensor:
+        """
+        Clip the values of the data to the specified range.
+        """
+        return torch.clamp(x_tilde, self.lower, self.upper)
 
 class Lp(ThreatModel):
     """
--- tests/test_adversarial_training.py
+++ tests/test_adversarial_training.py
@@ -6,8 +6,13 @@ def test_at(trainloader, testloader, device):
     # load model
     model = models.load('pytorch/vision', 'resnet18').to(device)
 
+    # define threat model
+    threat_model = threats.Composite([
+        threats.Linf(.03),
+        threats.Bounds(0, 1)])
+
     # initialize defense
-    defense = defenses.AdversarialTraining(trainloader, testloader, threats.Linf(.03), 10, 10, device)
+    defense = defenses.AdversarialTraining(trainloader, testloader, threat_model, 2, 2, device)
 
     # adversarially train
     robust_model = defense.apply(model)
