5d1584a43737dfa17409813d92691a0d61d139c5
Jonathan Peck <jonathan.peck@ugent.be>
2025-01-01 21:28:51 +0100

    list catalogs

--- dashboard/app.py
+++ dashboard/app.py
@@ -2,11 +2,13 @@ import dash
 
 import dash_bootstrap_components as dbc
 
-from dash import Input, Output, dcc, html
+import pandas as pd
+
+from dash import Input, Output, dcc, html, dash_table
 
 from adapter import Adapter
 
-adapter = Adapter('sqlite:///robusthub.db')
+adapter = Adapter('sqlite:///../robusthub.db')
 
 app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
 
@@ -27,6 +29,19 @@ app.layout = html.Div(
     ]
 )
 
+def create_catalog(data_dict: dict):
+    data_df = pd.DataFrame(data_dict)
+    data_catalog = dash_table.DataTable(
+        data=data_df.to_dict('records'),
+        columns=[{'name': i, 'id': i} for i in data_df.columns],
+        style_cell={'textAlign': 'left'},
+        style_header={
+            'fontWeight': 'bold'
+        },
+        style_as_list_view=True
+    )
+
+    return data_catalog
 
 @app.callback(Output("page-content", "children"), [Input("url", "pathname")])
 def render_page_content(pathname):
@@ -59,12 +74,56 @@ def render_page_content(pathname):
             className="p-3 bg-body-secondary rounded-3",
         )
 
+        model_title = html.H2('Model catalog')
+        models = adapter.load_models()
+        models_dict = {
+            'Name': [model.name for model in models],
+            'Repository': [model.repo for model in models],
+            'Tags': ['none' for model in models]
+        }
+        model_catalog = create_catalog(models_dict)
+
+        defense_title = html.H2('Defense catalog')
+        defenses = adapter.load_defenses()
+        defenses_dict = {
+            'Name': [defense.name for defense in defenses],
+            'Repository': [defense.repo for defense in defenses],
+            'Tags': ['none' for defense in defenses]
+        }
+        defense_catalog = create_catalog(defenses_dict)
+
+        attacks_title = html.H2('Attack catalog')
+        attacks = adapter.load_defenses()
+        attacks_dict = {
+            'Name': [attack.name for attack in attacks],
+            'Repository': [attack.repo for attack in attacks],
+            'Tags': ['none' for attack in attacks]
+        }
+        attacks_catalog = create_catalog(attacks_dict)
+
         main_content = html.Div(
             [
                 dbc.Row(
                     dbc.Col(jumbotron)
                 ),
-                dbc.Row(dbc.Col(html.Div("A single, half-width column"), width=6))
+                dbc.Row(
+                    dbc.Col(model_title)
+                ),
+                dbc.Row(
+                    dbc.Col(model_catalog)
+                ),
+                dbc.Row(
+                    dbc.Col(defense_title)
+                ),
+                dbc.Row(
+                    dbc.Col(defense_catalog)
+                ),
+                dbc.Row(
+                    dbc.Col(attacks_title)
+                ),
+                dbc.Row(
+                    dbc.Col(attacks_catalog)
+                )
             ]
         )
 
