16b89eebdea1bf6708d7423af0102ce3bb2bead0
Jonathan Peck <jonathan.peck@ugent.be>
2025-01-07 13:20:05 +0100

    populate attacks and defenses

---
+++ alembic/versions/4556382f93d1_populate_attacks.py
@@ -0,0 +1,35 @@
+"""populate attacks
+
+Revision ID: 4556382f93d1
+Revises: 8d9005e5e5cc
+Create Date: 2025-01-07 13:05:11.699358
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '4556382f93d1'
+down_revision: Union[str, None] = '8d9005e5e5cc'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+attacks = [
+    {'name': 'pgd', 'title': 'Projected gradient descent', 'repo': 'peck94/robusthub', 'arguments': '{}'},
+    {'name': 'fgsm', 'title': 'Fast gradient sign', 'repo': 'peck94/robusthub', 'arguments': '{}'}
+]
+
+def upgrade() -> None:
+    for attack in attacks:
+        name = attack['name']
+        title = attack['title']
+        repo = attack['repo']
+        args = attack['arguments']
+        op.execute(f"insert into attacks (name, title, repo, arguments) values ('{name}', '{title}', '{repo}', '{args}')")
+
+
+def downgrade() -> None:
+    op.execute("delete from attacks")
---
+++ alembic/versions/d4ca2c28cf22_populate_defenses.py
@@ -0,0 +1,35 @@
+"""populate defenses
+
+Revision ID: d4ca2c28cf22
+Revises: 4556382f93d1
+Create Date: 2025-01-07 13:17:14.636495
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = 'd4ca2c28cf22'
+down_revision: Union[str, None] = '4556382f93d1'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+defenses = [
+    {'name': 'adversarial_training', 'title': 'Adversarial training', 'repo': 'peck94/robusthub', 'arguments': '{}'},
+    {'name': 'randomized_smoothing', 'title': 'Randomized smoothing', 'repo': 'peck94/robusthub', 'arguments': '{}'}
+]
+
+def upgrade() -> None:
+    for defense in defenses:
+        name = defense['name']
+        title = defense['title']
+        repo = defense['repo']
+        args = defense['arguments']
+        op.execute(f"insert into defenses (name, title, repo, arguments) values ('{name}', '{title}', '{repo}', '{args}')")
+
+
+def downgrade() -> None:
+    op.execute("delete from defenses")
