7a765a71f792144c63463c4389ffa2c61c97595d
Jonathan Peck <jonathan.peck@ugent.be>
2025-01-28 19:44:54 +0100

    summaries

--- dashboard/pages/usecase.py
+++ dashboard/pages/usecase.py
@@ -40,23 +40,36 @@ def get_summaries(benchmarks: List[Benchmark]) -> Figure:
                     'bounds': values['bounds']
                 }
 
-            plots[metric]['standard'].append(values['standard']['mean'])
-            plots[metric]['standard_err'].append(values['standard']['err'])
-            plots[metric]['robust'].append(values['robust']['mean'])
-            plots[metric]['robust_err'].append(values['robust']['err'])
+            plots[metric]['standard'].append(float(values['standard']['mean']))
+            plots[metric]['standard_err'].append(float(values['standard']['err']))
+            plots[metric]['robust'].append(float(values['robust']['mean']))
+            plots[metric]['robust_err'].append(float(values['robust']['err']))
 
     fig = make_subplots(rows=len(plots), cols=1, subplot_titles=sorted(plots.keys()))
-    for metric in sorted(plots.keys()):
+    for i, metric in enumerate(sorted(plots.keys())):
         data = plots[metric]
         fig.add_trace(
             go.Scatter(
                 x=data['standard'],
                 y=data['robust'],
+                error_x=dict(
+                    type='data',
+                    array=data['standard_err'],
+                    visible=True
+                ),
+                error_y=dict(
+                    type='data',
+                    array=data['robust_err'],
+                    visible=True
+                ),
                 mode='markers',
                 hovertemplate='<b>Standard:</b> %{x:.2f}<br><b>Robust:</b> %{y:.2f}',
-                showlegend=False
+                showlegend=False,
+                name=metric
             )
         )
+        fig.update_xaxes(range=data['bounds'], title='Standard', row=i+1, col=1)
+        fig.update_yaxes(range=data['bounds'], title='Robust', row=i+1, col=1)
     return fig
 
 def layout(usecase_id=0, **kwargs):
